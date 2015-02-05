4404a461d7e3cafdcc97789a4ecea90530f579e3
Matt Jaffee <jaffee@umbel.com>
2015-02-04 19:53:38 -0600

    get issues from commit messages and annotate response

--- pigeon/views.py
+++ pigeon/views.py
@@ -1,7 +1,24 @@
 from pigeon import app
+import re
 from flask import request
 import json
 
+JIRA_PROJECTS = app.config['JIRA_PROJECTS']
+JIRA_URL = app.config['JIRA_URL']
+
+
 @app.route('/push', methods=["POST"])
 def new_push():
-    import ipdb; ipdb.set_trace()
+    data = json.loads(request.data)
+
+    for commit in data.get('commits', []):
+        issues = get_message_issues(JIRA_PROJECTS, commit.get('message', ""))
+        commit["issues"] = issues
+
+    return json.dumps(data)
+
+
+def get_message_issues(projects, msg):
+    proj_search_str = "(?:"+"|".join(projects)+")"
+    regex = "%s-[0-9]+" % proj_search_str
+    return re.findall(regex, msg)
